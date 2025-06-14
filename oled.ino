8a4b72be5e0e2961e6a57b0308e3734e5c12b7a8
Ausaus <51758@yupparaj.ac.th>
2025-06-14 14:38:18 +0700

    Update oled.ino

--- oled.ino
+++ oled.ino
@@ -13,17 +13,9 @@ void LightCheck()
   oled.clear(); //เคลียร์หน้าจอ
   oled.textSize(1); //ขนาดตัวอักษร
   oled.mode(0);
-  // oled.text(0,0,"FL = %d",FL);
-  // oled.text(1,0,"C  = %d",C);
-  // oled.text(2,0,"FR = %d",FR);
-  // oled.text(3,0,"BL = %d",BL);
-  // oled.text(4,0,"BR = %d",BR);
 
-  oled.text(0,0,"L3 = %d ",  L3);
-  oled.text(1,0,"L2 = %d ",  L2);
-  oled.text(2,0,"L1 = %d ",  L1);
-  oled.text(3,0,"R1 = %d ",  R1);
-  oled.text(4,0,"R2 = %d ",  R2);
-  oled.text(5,0,"R3 = %d ",  R3);
+  oled.text(0,0,"Left = %d ",  left_sensor);
+  oled.text(1,0,"Right = %d ",  right_sensor);
+  oled.text(2,0,"Front = %d ",  front_sensor);
   oled.show();
 }
